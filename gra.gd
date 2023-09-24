b89d6b6b74229931362c3fa7f633f364ad6cfa1d
Xander-Lambright <xanderlambright@gmail.com>
2023-09-23 23:07:30 -0700

    Removed Superflous Files

--- gra.gd
+++
@@ -1,9 +0,0 @@
-extends GraphEdit
-
-
-func _ready():
-	for i in 1050:
-		var node = GraphNode.new()
-		node.global_position = Vector2(randf_range(0,50),randf_range(0,50))
-		node.visible = true
-		add_child(node)
--- treetest.gd
+++
@@ -1,8 +0,0 @@
-extends Tree
-
-
-func _ready():
-	var root = create_item()
-	root.set_text(0,"test")
-	create_item(root).set_text(0,"test")
-	create_item(root)
