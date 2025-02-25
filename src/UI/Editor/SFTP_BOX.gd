ed99893cdb164dbc6e5ceb03c6616070f1c2e501
Xander-Lambright <xanderlambright@gmail.com>
2025-02-24 20:43:36 -0800

    messing with ui stuff

--- src/UI/Editor/SFTP_BOX.gd
+++ src/UI/Editor/SFTP_BOX.gd
@@ -7,10 +7,24 @@ func _ready():
 		$SftpConnection.self_modulate = Color(1,1,1,1)
 		$SftpDirectory.self_modulate = Color(1,1,1,1)
 		$SFTPLabel.self_modulate = Color(1,1,1,1)
-		$SftpConnection.text = CurrentEnvironment.sftp_client.ConnectionInfoDict["username"]+"@"+CurrentEnvironment.sftp_client.ConnectionInfoDict["hostname"]
-		$SftpDirectory.text = CurrentEnvironment.sftp_directory
+		$SftpConnection.text = "Server: "+CurrentEnvironment.sftp_client.ConnectionInfoDict["username"]+"@"+CurrentEnvironment.sftp_client.ConnectionInfoDict["hostname"]
+		$SftpDirectory.text = "Working SFTP Directory: "+CurrentEnvironment.sftp_directory
+		CurrentEnvironment.sftp_client.connect("SftpNotConnected",Callable(self,"_on_sftp_disconnected"))
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func _on_sftp_disconnected():
+	$SftpConnection.text = "SFTP Status : Disconnected"
+	$Button.visible = true
+	pass
+
+
+func _on_button_pressed():
+	CurrentEnvironment.sftp_client.ConnectToSftpServer(CurrentEnvironment.sftp_client.ConnectionInfoDict)
+	if CurrentEnvironment.sftp_client.Connected:
+		CurrentEnvironment.sftp_client.ChangeDirectory(CurrentEnvironment.sftp_client.remote_file_directory)
+		$SftpConnection.text = "SFTP Status : Connected"
+		
